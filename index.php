52be53c999c970d4c7573a1da197c3768d0b9a7e
Jean PRAT <jean.prat@orange.com>
2015-01-15 14:20:01 +0100

    script, style and footer removed

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$url="http://estcequecestbientotleweekend.fr/";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+$data = curl_exec($ch);
+curl_close($ch);
+$data=preg_replace('/<style.*<\/style>/s','',$data);
+$data=preg_replace('/<p id=\'footer\'>.*<\/script>/s','',$data);
+print_r($data);
+
+?>
