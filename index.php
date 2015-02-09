b5d7f335bd06b9752bbf35e32c7de4b593298fda
Jean PRAT <jean.prat@orange.com>
2015-02-09 09:35:29 +0100

    suppression balises script

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 $data = curl_exec($ch);
 curl_close($ch);
+$data=preg_replace('/<script[^<]*<\/script>/s','',$data);
 $data=preg_replace('/<style.*<\/style>/s','',$data);
 $data=preg_replace('/<p id=\'footer\'>.*<\/script>/s','',$data);
 print_r($data);
